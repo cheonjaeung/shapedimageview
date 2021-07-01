03f24d92c6f519a384017abbb710843f38188101
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-07-01 11:28:21 +0900

    feat: implement drawable to bitmap conversion

---
+++ shapedimageview/src/main/java/io/woong/shapedimageview/ShapedImageView.kt
@@ -0,0 +1,39 @@
+package io.woong.shapedimageview
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+abstract class ShapedImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : AppCompatImageView(context, attrs, defStyle) {
+
+    private var image: Bitmap? = null
+    private var imageCache: Drawable? = null
+    private var imagePaint: Paint = Paint().apply {
+        isAntiAlias = true
+        isDither = true
+        alpha = 255
+    }
+
+    private fun drawableToBitmap(drawable: Drawable): Bitmap {
+        return if (drawable is BitmapDrawable) {
+            drawable.bitmap
+        } else {
+            val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            drawable.apply {
+                setBounds(0, 0, canvas.width, canvas.height)
+                draw(canvas)
+            }
+            bitmap
+        }
+    }
+}
