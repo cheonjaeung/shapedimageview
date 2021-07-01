7aafb29a54421866db70ccf2bca36a5aac7c396b
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-07-01 13:53:14 +0900

    feat: implement scale type filtering

--- shapedimageview/src/main/java/io/woong/shapedimageview/ShapedImageView.kt
+++ shapedimageview/src/main/java/io/woong/shapedimageview/ShapedImageView.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import java.lang.IllegalArgumentException
 
 abstract class ShapedImageView @JvmOverloads constructor(
     context: Context,
@@ -74,12 +75,8 @@ abstract class ShapedImageView @JvmOverloads constructor(
             val shader = BitmapShader(img, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP).apply {
                 setLocalMatrix(
                     when (scaleType) {
-                        ScaleType.CENTER_CROP -> {
-                            createCenterCropMatrix(img, imageWidth, imageHeight)
-                        }
-                        else -> {
-                            Matrix()
-                        }
+                        ScaleType.CENTER_CROP -> createCenterCropMatrix(img, imageWidth, imageHeight)
+                        else -> Matrix()
                     }
                 )
             }
@@ -116,6 +113,25 @@ abstract class ShapedImageView @JvmOverloads constructor(
 
             setScale(scale, scale)
             postTranslate(dx, dy)
+        }
+    }
+
+    /**
+     * Set scale type of this image view.
+     *
+     * Only can accept [CENTER_CROP][android.widget.ImageView.ScaleType.CENTER_CROP],
+     * Other scale types will be denied.
+     *
+     * @param scaleType A scale type mode.
+     *
+     * @throws IllegalArgumentException When given scale type is not supported.
+     */
+    override fun setScaleType(scaleType: ScaleType) {
+        when (scaleType) {
+            ScaleType.CENTER_CROP -> {
+                super.setScaleType(scaleType)
+            }
+            else -> throw IllegalArgumentException("ScaleType $scaleType not supported.")
         }
     }
 }
