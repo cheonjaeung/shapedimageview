454da21123b88c0b5b1164c0b7fe3354813f604c
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-07-01 14:39:18 +0900

    feat: make circular image view default scale type to center-crop

--- shapedimageview/src/main/java/io/woong/shapedimageview/CircularImageView.kt
+++ shapedimageview/src/main/java/io/woong/shapedimageview/CircularImageView.kt
@@ -3,6 +3,7 @@ package io.woong.shapedimageview
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
+import java.lang.IllegalArgumentException
 import kotlin.math.min
 
 class CircularImageView @JvmOverloads constructor(
@@ -14,6 +15,10 @@ class CircularImageView @JvmOverloads constructor(
     private var imageCenterX: Float = 0f
     private var imageCenterY: Float = 0f
     private var imageRadius: Float = 0f
+
+    init {
+        scaleType = ScaleType.CENTER_CROP
+    }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val w = MeasureSpec.getSize(widthMeasureSpec)
@@ -35,5 +40,24 @@ class CircularImageView @JvmOverloads constructor(
         updateShader((imageRadius * 2).toInt(), (imageRadius * 2).toInt())
 
         canvas.drawCircle(imageCenterX, imageCenterY, imageRadius, imagePaint)
+    }
+
+    /**
+     * Set scale type of this image view.
+     *
+     * Only can accept [CENTER_CROP][android.widget.ImageView.ScaleType.CENTER_CROP],
+     * Other scale types will be denied.
+     *
+     * @param scaleType A scale type mode.
+     *
+     * @throws IllegalArgumentException When given scale type is not supported.
+     */
+    override fun setScaleType(scaleType: ScaleType) {
+        when (scaleType) {
+            ScaleType.CENTER_CROP -> {
+                super.setScaleType(scaleType)
+            }
+            else -> throw IllegalArgumentException("ScaleType $scaleType not supported.")
+        }
     }
 }
