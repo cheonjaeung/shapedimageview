157159041488e865afcdc025dad372b70dee6563
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-07-04 21:11:42 +0900

    feat: create squircular image view class

---
+++ shapedimageview/src/main/java/io/woong/shapedimageview/SquircularImageView.kt
@@ -0,0 +1,34 @@
+package io.woong.shapedimageview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+
+class SquircularImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : ShapedImageView(context, attrs, defStyle) {
+
+    /**
+     * This method is invoked after [onMeasure].
+     *
+     * @param widthMeasureSpec Specs of width.
+     * You can access mode and size as [MeasureSpec][android.view.View.MeasureSpec].
+     * @param heightMeasureSpec Specs of height.
+     * You can access mode and size as [MeasureSpec][android.view.View.MeasureSpec].
+     * @param size Size of view. (width and height is same)
+     */
+    override fun postOnMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int, size: Int) {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * This method is invoked after [onDraw].
+     *
+     * @param canvas Canvas to draw image view.
+     */
+    override fun postOnDraw(canvas: Canvas) {
+        TODO("Not yet implemented")
+    }
+}
