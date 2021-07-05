be3d7d1f701e8307ae2607ec3d6872e2af99226b
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-07-05 17:17:05 +0900

    fix: remove attribute apply method in abstract class
    cuz, can't use view preview in android studio when apply attributes in abstract class.

--- shapedimageview/src/main/java/io/woong/shapedimageview/ShapedImageView.kt
+++ shapedimageview/src/main/java/io/woong/shapedimageview/ShapedImageView.kt
@@ -42,25 +42,6 @@ abstract class ShapedImageView @JvmOverloads constructor(
 
     init {
         scaleType = ScaleType.CENTER_CROP
-        applyAttributes(attrs, defStyle)
-    }
-
-    /**
-     * Apply custom attributes.
-     */
-    private fun applyAttributes(attributes: AttributeSet?, defStyle: Int) {
-        val attrs = context.obtainStyledAttributes(
-            attributes,
-            R.styleable.ShapedImageView,
-            defStyle,
-            0
-        )
-
-        try {
-
-        } finally {
-            attrs.recycle()
-        }
     }
 
     /**
