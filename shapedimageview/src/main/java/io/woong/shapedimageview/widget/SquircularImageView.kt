983a1801f861acbbea577dee7ca6f1cf687f215c
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-07-05 21:11:08 +0900

    feat: implement squircular image drawing

--- shapedimageview/src/main/java/io/woong/shapedimageview/widget/SquircularImageView.kt
+++ shapedimageview/src/main/java/io/woong/shapedimageview/widget/SquircularImageView.kt
@@ -5,10 +5,13 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.util.Log
 import io.woong.shapedimageview.ShapedImageView
+import io.woong.shapedimageview.util.SuperellipseFormula
 
 /**
- * An image view that displaying image in [squircle](https://en.wikipedia.org/wiki/Squircle) shape.
+ * An image view that displaying image in [squircle](https://en.wikipedia.org/wiki/Squircle) shape
+ * (Squircle is a kind of [superellipse](https://en.wikipedia.org/wiki/Superellipse)).
  *
  * Scale type is always [center crop][android.widget.ImageView.ScaleType.CENTER_CROP].
  * And also it's width and height size is same.
@@ -19,10 +22,10 @@ class SquircularImageView @JvmOverloads constructor(
     defStyle: Int = 0
 ) : ShapedImageView(context, attrs, defStyle) {
 
-    /** X position of image left end. */
-    private var imageLeft: Float = 0f
-    /** Y position of image top end. */
-    private var imageTop: Float = 0f
+    /** X position of image center. */
+    private var imageCenterX: Float = 0f
+    /** Y position of image center. */
+    private var imageCenterY: Float = 0f
     /** Radius size of image. */
     private var imageRadius: Float = 0f
 
@@ -35,9 +38,9 @@ class SquircularImageView @JvmOverloads constructor(
      * You can access mode and size as [MeasureSpec][android.view.View.MeasureSpec].
      */
     override fun postOnMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int, size: Int) {
-        imageLeft = paddingLeft.toFloat()
-        imageTop = paddingTop.toFloat()
         imageRadius = imageSize / 2f
+        imageCenterX = (paddingLeft + (size - paddingRight)) / 2f
+        imageCenterY = (paddingTop + (size - paddingBottom)) / 2f
     }
 
     /**
@@ -46,22 +49,28 @@ class SquircularImageView @JvmOverloads constructor(
      * @param canvas Canvas to draw image view.
      */
     override fun postOnDraw(canvas: Canvas) {
-        canvas.drawSquircle(imageLeft, imageTop, imageSize.toFloat(), imageRadius, imagePaint)
+        canvas.drawSquircle(imageCenterX, imageCenterY, imageRadius, imagePaint)
     }
 
     /**
      * Draw the squircle using the specified paint.
      * The squircle will be filled or framed based on the Style in the paint.
      */
-    private fun Canvas.drawSquircle(left: Float, top: Float, size: Float, radius: Float, paint: Paint) {
-        val startX = left
-        val startY = top + radius
+    private fun Canvas.drawSquircle(cx: Float, cy: Float, radius: Float, paint: Paint) {
+        val formula = SuperellipseFormula(cx, cy, radius, 3f)
 
-        val path = Path().apply {
-            moveTo(startX, startY)
-
-            close()
+        val path = Path()
+        path.apply {
+            moveTo(formula.x, formula.y)
+            for (angle in 0 until 360) {
+                formula.angle = angle + 1
+                val x = formula.x
+                val y = formula.y
+                if (x.isNaN() || y.isNaN()) break
+                lineTo(x, y)
+            }
         }
+        path.close()
 
         this.drawPath(path, paint)
     }
