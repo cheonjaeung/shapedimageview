42bfafa17c2c79b4d326b3952f26396f21c4e60c
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-09-26 15:05:48 +0900

    demo: redesign demo application

--- app/src/main/java/io/woong/shapedimageviewdemo/MainActivity.kt
+++ app/src/main/java/io/woong/shapedimageviewdemo/MainActivity.kt
@@ -1,120 +1,189 @@
 package io.woong.shapedimageviewdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.SeekBar
-import androidx.appcompat.widget.AppCompatSeekBar
-import androidx.core.view.setPadding
-import io.woong.shapedimageview.widget.CircularImageView
-import io.woong.shapedimageview.widget.RoundedSquareImageView
-import io.woong.shapedimageview.widget.SquareImageView
-import io.woong.shapedimageview.widget.SquircularImageView
+import androidx.appcompat.app.AppCompatActivity
+import io.woong.shapedimageview.ShapedImageView
+import io.woong.shapedimageview.formula.SuperEllipseFormula
+import io.woong.shapedimageview.widget.CutCornerImageView
+import io.woong.shapedimageview.widget.FormulableImageView
+import io.woong.shapedimageview.widget.OvalImageView
+import io.woong.shapedimageview.widget.RoundImageView
 
-class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
+class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener {
 
-    private lateinit var circleView: CircularImageView
-    private lateinit var roundedSquareView: RoundedSquareImageView
-    private lateinit var squareView: SquareImageView
-    private lateinit var squircleView: SquircularImageView
+    private val oval: OvalImageView by lazy { findViewById(R.id.oval) }
+    private val round: RoundImageView by lazy { findViewById(R.id.round) }
+    private val cutCorner: CutCornerImageView by lazy { findViewById(R.id.cut_corner) }
+    private val formulable: FormulableImageView by lazy { findViewById(R.id.formulable) }
+
+    private val shapeGroup: RadioGroup by lazy { findViewById(R.id.shape_group) }
+    private val ovalShapeButton: RadioButton by lazy { findViewById(R.id.oval_shape) }
+    private val roundShapeButton: RadioButton by lazy { findViewById(R.id.round_shape) }
+    private val cutCornerShapeButton: RadioButton by lazy { findViewById(R.id.cut_corner_shape) }
+    private val formulableShapeButton: RadioButton by lazy { findViewById(R.id.formulable_shape) }
+
+    private val widthSeekBar: SeekBar by lazy { findViewById(R.id.width_seekbar) }
+    private val heightSeekBar: SeekBar by lazy { findViewById(R.id.height_seekbar) }
+    private val paddingSeekBar: SeekBar by lazy { findViewById(R.id.padding_seekbar) }
+    private val borderSizeSeekBar: SeekBar by lazy { findViewById(R.id.border_size_seekbar) }
+    private val shadowSizeSeekBar: SeekBar by lazy { findViewById(R.id.shadow_size_seekbar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        circleView = findViewById(R.id.circle)
-        roundedSquareView = findViewById(R.id.rounded_square)
-        squareView = findViewById(R.id.square)
-        squircleView = findViewById(R.id.squircle)
+        shapeGroup.setOnCheckedChangeListener(this)
 
-        findViewById<AppCompatSeekBar>(R.id.padding_seekbar).apply {
+        formulable.formula = SuperEllipseFormula(3f)
+
+        widthSeekBar.apply {
+            max = 200
+            progress = 100
             setOnSeekBarChangeListener(this@MainActivity)
+        }
+        heightSeekBar.apply {
+            max = 200
+            progress = 100
+            setOnSeekBarChangeListener(this@MainActivity)
+        }
+        paddingSeekBar.apply {
+            max = 16
             progress = 0
-        }
-
-        findViewById<AppCompatSeekBar>(R.id.shadow_seekbar).apply {
             setOnSeekBarChangeListener(this@MainActivity)
-            progress = 4
         }
-
-        findViewById<AppCompatSeekBar>(R.id.border_seekbar).apply {
+        borderSizeSeekBar.apply {
+            max = 8
+            progress = 0
             setOnSeekBarChangeListener(this@MainActivity)
-            progress = 8
         }
-
-        findViewById<AppCompatSeekBar>(R.id.values_seekbar).apply {
+        shadowSizeSeekBar.apply {
+            max = 8
+            progress = 0
             setOnSeekBarChangeListener(this@MainActivity)
-            progress = 75
         }
     }
 
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        if (group?.id == shapeGroup.id) {
+            val visible = View.VISIBLE
+            val gone = View.GONE
+
+            when (checkedId) {
+                ovalShapeButton.id -> {
+                    oval.visibility = visible
+                    round.visibility = gone
+                    cutCorner.visibility = gone
+                    formulable.visibility = gone
+                }
+                roundShapeButton.id -> {
+                    oval.visibility = gone
+                    round.visibility = visible
+                    cutCorner.visibility = gone
+                    formulable.visibility = gone
+                }
+                cutCornerShapeButton.id -> {
+                    oval.visibility = gone
+                    round.visibility = gone
+                    cutCorner.visibility = visible
+                    formulable.visibility = gone
+                }
+                formulableShapeButton.id -> {
+                    oval.visibility = gone
+                    round.visibility = gone
+                    cutCorner.visibility = gone
+                    formulable.visibility = visible
+                }
+            }
+        }
+    }
+
+    // Do Nothing
     override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
         when (seekBar?.id) {
-            R.id.padding_seekbar -> {
-                changePaddings(progress)
+            widthSeekBar.id -> {
+                val newSize = (seekBar.progress + 100).dp
+                changeWidth(oval, newSize)
+                changeWidth(round, newSize)
+                changeWidth(cutCorner, newSize)
+                changeWidth(formulable, newSize)
             }
-            R.id.shadow_seekbar -> {
-                changeShadows(progress.toFloat())
+            heightSeekBar.id -> {
+                val newSize = (seekBar.progress + 100).dp
+                changeHeight(oval, newSize)
+                changeHeight(round, newSize)
+                changeHeight(cutCorner, newSize)
+                changeHeight(formulable, newSize)
             }
-            R.id.border_seekbar -> {
-                changeBorders(progress.toFloat())
+            paddingSeekBar.id -> {
+                val newPadding = seekBar.progress.dp
+                changePadding(oval, newPadding)
+                changePadding(round, newPadding)
+                changePadding(cutCorner, newPadding)
+                changePadding(formulable, newPadding)
             }
-            R.id.values_seekbar -> {
-                changeValues(progress.toFloat())
+            borderSizeSeekBar.id -> {
+                val newSize = seekBar.progress.dpf
+                changeBorderSize(oval, newSize)
+                changeBorderSize(round, newSize)
+                changeBorderSize(cutCorner, newSize)
+                changeBorderSize(formulable, newSize)
+            }
+            shadowSizeSeekBar.id -> {
+                val newSize = seekBar.progress.dpf
+                changeShadowSize(oval, newSize)
+                changeShadowSize(round, newSize)
+                changeShadowSize(cutCorner, newSize)
+                changeShadowSize(formulable, newSize)
             }
         }
     }
 
-    override fun onStartTrackingTouch(seekBar: SeekBar?) { /* Not needed. */ }
+    // Do Nothing
+    override fun onStartTrackingTouch(seekBar: SeekBar?) { }
 
-    override fun onStopTrackingTouch(seekBar: SeekBar?) { /* Not needed. */ }
+    // Do Nothing
+    override fun onStopTrackingTouch(seekBar: SeekBar?) { }
 
-    private fun changePaddings(padding: Int) {
-        val p = TypedValue.applyDimension(
+    private val Int.dp: Int
+        get() = TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
-            padding.toFloat(),
+            this.toFloat(),
             resources.displayMetrics
         ).toInt()
 
-        circleView.setPadding(p)
-        roundedSquareView.setPadding(p)
-        squareView.setPadding(p)
-        squircleView.setPadding(p)
-    }
-
-    private fun changeShadows(shadowSize: Float) {
-        val s = TypedValue.applyDimension(
+    private val Int.dpf: Float
+        get() = TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
-            shadowSize,
+            this.toFloat(),
             resources.displayMetrics
         )
 
-        circleView.shadowSize = s
-        roundedSquareView.shadowSize = s
-        squareView.shadowSize = s
-        squircleView.shadowSize = s
+    private fun changeWidth(view: View, newSize: Int) {
+        val params = view.layoutParams
+        params.width = newSize
+        view.layoutParams = params
     }
 
-    private fun changeBorders(borderSize: Float) {
-        val b = TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            borderSize,
-            resources.displayMetrics
-        )
-
-        circleView.borderSize = b
-        roundedSquareView.borderSize = b
-        squareView.borderSize = b
-        squircleView.borderSize = b
+    private fun changeHeight(view: View, newSize: Int) {
+        val params = view.layoutParams
+        params.height = newSize
+        view.layoutParams = params
     }
 
-    private fun changeValues(value: Float) {
-        roundedSquareView.imageRadius = TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            0.48f * value,
-            resources.displayMetrics
-        )
+    private fun changePadding(view: View, newSize: Int) {
+        view.setPadding(newSize, newSize, newSize, newSize)
+    }
 
-        squircleView.imageCurvature = value / 25 + 1
+    private fun changeBorderSize(view: ShapedImageView, newSize: Float) {
+        view.borderSize = newSize
+    }
+
+    private fun changeShadowSize(view: ShapedImageView, newSize: Float) {
+        view.shadowSize = newSize
     }
 }
