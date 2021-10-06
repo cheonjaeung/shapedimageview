ed5440cb037da308b834b405d229e6b438af9bc4
Jaewoong Cheon <cheonjaewoong@gmail.com>
2021-10-06 22:38:29 +0900

    feat: implement center and center-inside scale type matrix

--- shapedimageview/src/main/java/io/woong/shapedimageview/util/MatrixUtil.kt
+++ shapedimageview/src/main/java/io/woong/shapedimageview/util/MatrixUtil.kt
@@ -121,6 +121,27 @@ internal fun createFitEndMatrix(img: Bitmap, b: Bounds): Matrix = Matrix().apply
 }
 
 /**
+ * Create a bitmap shader matrix for center scale type.
+ *
+ * @param img The bitmap object to be drawn.
+ * @param b The [Bounds] object that containing bounds of imageview.
+ *
+ * @return A matrix object for center scale type.
+ */
+@RequiresApi(api = 31)
+internal fun createCenterMatrix(img: Bitmap, b: Bounds): Matrix = Matrix().apply {
+    val w = b.width - b.paddingHorizontal - (b.borderSize * 2) - (b.shadowSize * 2)
+    val h = b.height - b.paddingVertical - (b.borderSize * 2) - (b.shadowSize * 2)
+    val iw = img.width.toFloat()
+    val ih = img.height.toFloat()
+
+    val dx = (w - iw) / 2 + b.paddingLeft + b.borderSize + b.shadowSize
+    val dy = (h - ih) / 2 + b.paddingTop + b.borderSize + b.shadowSize
+    postScale(1f, 1f)
+    postTranslate(dx, dy)
+}
+
+/**
  * Create a bitmap shader matrix for center-crop scale type.
  *
  * @param img The bitmap object to be drawn.
@@ -153,6 +174,47 @@ internal fun createCenterCropMatrix(img: Bitmap, b: Bounds): Matrix = Matrix().a
         scale = h / ih
         dx = (w - (iw * scale)) / 2 + b.paddingLeft + b.borderSize + b.shadowSize
         dy = b.paddingTop + b.borderSize + b.shadowSize
+    }
+    postScale(scale, scale)
+    postTranslate(dx, dy)
+}
+
+/**
+ * Create a bitmap shader matrix for center-inside scale type.
+ *
+ * @param img The bitmap object to be drawn.
+ * @param b The [Bounds] object that containing bounds of imageview.
+ *
+ * @return A matrix object for center-inside scale type.
+ */
+@RequiresApi(api = 31)
+internal fun createCenterInsideMatrix(img: Bitmap, b: Bounds): Matrix = Matrix().apply {
+    val w = b.width - b.paddingHorizontal - (b.borderSize * 2) - (b.shadowSize * 2)
+    val h = b.height - b.paddingVertical - (b.borderSize * 2) - (b.shadowSize * 2)
+    val iw = img.width.toFloat()
+    val ih = img.height.toFloat()
+    val ratio = w / h
+    val imgRatio = iw / ih
+
+    val scale: Float
+    val dx: Float
+    val dy: Float
+    when {
+        ratio < imgRatio && iw > w -> {
+            scale = w / iw
+            dx = b.paddingLeft + b.borderSize + b.shadowSize
+            dy = (h - (ih * scale)) / 2 + b.paddingTop + b.borderSize + b.shadowSize
+        }
+        ratio > imgRatio && ih > h -> {
+            scale = h / ih
+            dx = (w - (iw * scale)) / 2 + b.paddingLeft + b.borderSize + b.shadowSize
+            dy = b.paddingTop + b.borderSize + b.shadowSize
+        }
+        else -> {
+            scale = 1f
+            dx = (w - iw) / 2 + b.paddingLeft + b.borderSize + b.shadowSize
+            dy = (h - ih) / 2 + b.paddingTop + b.borderSize + b.shadowSize
+        }
     }
     postScale(scale, scale)
     postTranslate(dx, dy)
